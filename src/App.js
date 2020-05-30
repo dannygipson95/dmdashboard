60ce6db3019feec42497d45bdda934b063c057a5
Daniel Gipson <dannygipson95@gmail.com>
2020-05-30 16:01:40 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Hello world!
+          Hello world! 2
         </p>
         <a
           className="App-link"
